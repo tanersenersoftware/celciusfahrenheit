6c9272a5469ecce8d00c6d962c9430f59e06c62d
tsdatasciencist <tanersenerbusiness@gmail.com>
2024-04-04 12:40:18 +0300

    git commit -m ".gitignore deactive and ifelseelif1"

---
+++ ifelseelif1.py
@@ -0,0 +1,12 @@
+
+#^  response = input("Would you like food? yes/no: ")
+#^ if response == "yes":
+#^    print("Okey, let's preparing to foods with you.")
+#^ else:
+#^    print("Okey, i will don't preparing to foods")
+
+name = input("Enter your name: ")
+if name == "":
+    print("You did not type in your name!")
+else:
+    print(f"Hello {name}")
